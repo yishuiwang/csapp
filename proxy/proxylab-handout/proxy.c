bbb2e595d28cdf7fd0559953d001ac8e66e1f5bc
scruffy <yishuiwang163@gmail.com>
2023-08-22 15:25:09 +0800

    Update proxy.c

--- proxy/proxylab-handout/proxy.c
+++ proxy/proxylab-handout/proxy.c
@@ -5,10 +5,28 @@
 #define WEB_PREFIX "http://"
 #define NTHREADS 8
 
+#define MAX_CACHE_SIZE (1024 * 1024) // 1 MiB in bytes
+#define MAX_OBJECT_SIZE 1024         // 1 KiB in bytes
+#define CACHE_BLOCK_NUM (MAX_CACHE_SIZE / MAX_OBJECT_SIZE)
+
+typedef struct
+{
+    char object[MAX_OBJECT_SIZE];
+    char uri[MAXLINE];
+    int LRU;
+    int is_empty;
+
+    int read_cnt;
+    sem_t read_mutex;
+    sem_t write_mutex;
+} block;
+
+block cache[CACHE_BLOCK_NUM + 1];
+
 void handle_request(int connfd);
 void parse_uri(char *uri, char *filename, char *host, char *port);
 void read_requesthdrs(rio_t *rp);
-void forward_request(char *method, char *host, char *port, char *filename, int connfd);
+void forward_request(char *method, char *host, char *port, char *filename, int connfd, block *cache_block);
 void serve_static(int fd, char *filename, int filesize);
 void get_filetype(char *filename, char *filetype);
 void *thread(void *vargp);
@@ -22,6 +40,16 @@ int main(int argc, char *argv[])
     {
         fprintf(stderr, "usage: %s <port>\n", argv[0]);
         exit(1);
+    }
+
+    // init cache
+    for (int i = 0; i < CACHE_BLOCK_NUM; i++)
+    {
+        cache[i].is_empty = 1;
+        cache[i].LRU = 0;
+        cache[i].read_cnt = 0;
+        Sem_init(&cache[i].read_mutex, 0, 1);
+        Sem_init(&cache[i].write_mutex, 0, 1);
     }
 
     pthread_t tid[NTHREADS];
@@ -76,15 +104,58 @@ void handle_request(int connfd)
     if (strcasecmp(method, "GET"))
         unix_error("Proxy does not implement this method");
 
+    // check cache
+    char cache_uri[MAXLINE];
+    strcpy(cache_uri, uri);
+
+    int i;
+    if ((i = get_Cache(cache_uri)) != -1)
+    {
+        // 加锁
+        // P(&cache[i].read_mutex);
+        // cache[i].read_cnt++;
+        // if (cache[i].read_cnt == 1)
+        //     P(&cache[i].write_mutex);
+        // V(&cache[i].read_mutex);
+
+        Rio_writen(connfd, cache[i].object, strlen(cache[i].object));
+
+        // 解锁
+        // P(&cache[i].read_mutex);
+        // cache[i].read_cnt--;
+        // if (cache[i].read_cnt == 0)
+        //     V(&cache[i].write_mutex);
+        // V(&cache[i].read_mutex);
+
+        return;
+    }
+
+    // cache miss
+
+    block *cache_block = malloc(sizeof(block));
+    cache_block->is_empty = 0;
+    cache_block->LRU = 0;
+    strcpy(cache_block->uri, cache_uri);
+
     read_requesthdrs(&rio);
 
     parse_uri(uri, filename, host, port);
 
-    forward_request(method, host, port, filename, connfd);
+    forward_request(method, host, port, filename, connfd, cache_block);
+
+    for (int i = 0; i < CACHE_BLOCK_NUM; i++)
+    {
+        if (cache[i].is_empty == 1)
+        {
+            cache[i] = *cache_block;
+            break;
+        }
+    }
+
     return;
 }
 
-void forward_request(char *method, char *host, char *port, char *filename, int connfd)
+void forward_request(char *method, char *host, char *port, char *filename, int connfd, block *cache_block)
 {
     static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
     static const char *connection_hdr = "Connection: close\r\n";
@@ -116,13 +187,29 @@ void forward_request(char *method, char *host, char *port, char *filename, int c
     rio_t rio;
     Rio_readinitb(&rio, clientfd);
     Rio_writen(rio.rio_fd, request, strlen(request));
+    int size_buf = 0;
 
     int n;
+    int total_size = 0;
+    char cache_buf[MAX_OBJECT_SIZE];
     char tinyResponse[MAXLINE];
     while ((n = Rio_readlineb(&rio, tinyResponse, MAXLINE)) != 0)
     {
-        printf("%s", tinyResponse);
+        total_size += n;
+        if (total_size < MAX_OBJECT_SIZE)
+        {
+            strcat(cache_buf, tinyResponse);
+        }
         Rio_writen(connfd, tinyResponse, n);
+    }
+    Close(clientfd);
+    if (total_size < MAX_OBJECT_SIZE)
+    {
+        strcpy(cache_block->object, cache_buf);
+    }
+    else
+    {
+        cache_block->is_empty = 1;
     }
 }
 
@@ -189,4 +276,16 @@ void get_filetype(char *filename, char *filetype)
         strcpy(filetype, "image/jpeg");
     else
         strcpy(filetype, "text/plain");
+}
+
+int get_Cache(char *name)
+{
+    for (int i = 0; i < CACHE_BLOCK_NUM; i++)
+    {
+        if (cache[i].is_empty == 0 && strcmp(cache[i].uri, name) == 0)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
